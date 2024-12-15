f26e4c4393ad024a9b87eb6e9ad2a17e9f4389f1
Mahmoud-Elmasry77 <68786461+Mahmoud-Elmasry77@users.noreply.github.com>
2024-12-15 15:46:46 +0300

    Add files via upload

---
+++ User.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import "./User.css"
+function Login({ setAuthToken }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("https://jsonplaceholder.typicode.com/users", {
+        username,
+        password,
+      });
+      setAuthToken(response.data.token);
+    } catch (err) {
+      setError('Invalid username or password');
+    }
+  };
+
+  return (
+    <div className='login'>
+      <h2>Login</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
